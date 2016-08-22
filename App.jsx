b889dff1683ed5f7c185e48cdc556141133be25b
mohanprasath.s <mohanprasath.s@zohocorp.com>
2016-08-22 17:35:11 +0530

    state with lighting strikes

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LightningCounterDisplay from './components/LightingCounter.jsx'
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <LightningCounterDisplay></LightningCounterDisplay>
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ components/LightingCounter.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+var LightningCounter = React.createClass({
+  getInitialState: function() {
+    return {
+      strikes: 0
+    };
+  },
+  timerTick: function() {
+    this.setState({
+      strikes: this.state.strikes + 100
+    });
+  },
+  componentDidMount: function() {
+    setInterval(this.timerTick, 1000);
+  },
+  render: function() {
+    var counterStyle = {
+      color: "#66FFFF",
+      fontSize: 50
+    };
+ 
+    var count = this.state.strikes.toLocaleString();
+ 
+    return (
+      <h1 style={counterStyle}>{count}</h1>
+    );
+  }
+});
+ 
+var LightningCounterDisplay = React.createClass({
+    render: function() {
+      var commonStyle = {
+        margin: 0,
+        padding: 0
+      }
+      var divStyle = {
+        width: 250,
+        textAlign: "center",
+        backgroundColor: "#020202",
+        padding: 40,
+        fontFamily: "sans-serif",
+        color: "#999999",
+        borderRadius: 10
+      };
+ 
+      var textStyles = {
+        emphasis: {
+          fontSize: 38,
+        },
+        smallEmphasis: {
+        },
+        small: {
+          fontSize: 17,
+          opacity: 0.5,
+        }
+      }
+ 
+      return(
+        <div style={divStyle}>
+          <LightningCounter/>
+          <h2 style={textStyles.smallEmphasis}>LIGHTNING STRIKES</h2>
+          <h2 style={textStyles.emphasis}>WORLDWIDE</h2>
+          <p style={textStyles.small}>(since you loaded this example)</p>
+        </div>
+      );
+    }
+});
+ 
+ export default LightningCounterDisplay
+   
